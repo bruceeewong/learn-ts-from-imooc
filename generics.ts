0464415b636b93e4319ff427834fd4f72128fd9c
bruski <292803281@qq.com>
2020-10-15 18:06:54 +0800

    feat: add generics more sectoin

--- generics.ts
+++ generics.ts
@@ -2,6 +2,7 @@
 // function echo(arg: any): any {
 //   return arg;
 // }
+export {};
 
 // use generics
 function echo<T>(arg: T): T {
@@ -9,8 +10,76 @@ function echo<T>(arg: T): T {
 }
 
 const str: string = "a";
-const result = echo(str);
+let result: any = echo(str);
 
 function swap<T, U>(tuple: [T, U]): [U, T] {
   return [tuple[1], tuple[0]];
 }
+
+// generics array
+function echoWithArr<T>(arg: T[]): T[] {
+  console.log(arg.length);
+  return arg;
+}
+
+result = echoWithArr([1, 2, 3]);
+
+// generics with restriction
+// duck typing: allow type with length
+interface IWithLength {
+  length: number;
+}
+
+function echoWithLength<T extends IWithLength>(arg: T): T {
+  console.log(arg.length);
+  return arg;
+}
+
+result = echoWithLength("123");
+result = echoWithLength([1, 2, 3]);
+result = echoWithLength({ length: 10 });
+
+// generics define class
+class Queue<T> {
+  private data = [];
+  push(item: T) {
+    return this.data.push(item);
+  }
+  pop() {
+    return this.data.shift();
+  }
+}
+
+const queue = new Queue<number>();
+queue.push(1);
+// queue.push("a");  // type error
+
+const queue2 = new Queue<string>();
+queue2.push("a");
+// queue2.push(1)  // type error
+
+interface KeyPair<T, U> {
+  key: T;
+  value: U;
+}
+
+let kp1: KeyPair<number, string> = { key: 1, value: "str" };
+let kp2: KeyPair<string, number> = { key: "test", value: 100 };
+
+let arr: number[] = [1, 2, 3];
+let arrTwo: Array<number> = [1, 2, 3]; // use generics to describe typed array
+
+// use generics to describe func
+interface IPlus<T> {
+  (a: T, b: T): T;
+}
+function plus(a: number, b: number): number {
+  return a + b;
+}
+const a: IPlus<number> = plus;
+
+function connect(a: string, b: string): string {
+  return a + b;
+}
+
+const b: IPlus<string> = connect;
--- 学习TypeScript.md
+++ 学习TypeScript.md
@@ -251,7 +251,7 @@ const enum ConstDirectoin {
 }
 ```
 
-## 泛型 Generics
+### Generics 泛型 
 
 >  动机: 想要定义多类型的输入输出相同的函数，如echo
 >
@@ -275,6 +275,96 @@ function echo<T>(arg: T): T {
 function swap<T, U>(tuple: [T, U]): [U, T] {
   return [tuple[1], tuple[0]];
 }
+```
+
+泛型数组定义
+
+```
+// generics array
+function echoWithArr<T>(arg: T[]): T[] {
+  console.log(arg.length);
+  return arg;
+}
+
+result = echoWithArr([1, 2, 3]);
+```
+
+通过接口约束泛型：鸭子类型
+
+```
+interface IWithLength {
+  length: number;
+}
+
+function echoWithLength<T extends IWithLength>(arg: T): T {
+  console.log(arg.length);
+  return arg;
+}
+
+result = echoWithLength("123");
+result = echoWithLength([1, 2, 3]);
+result = echoWithLength({ length: 10 });
+```
+
+类和接口
+
+通过泛型，从调用处指定类型
+
+```
+/ generics define class
+class Queue<T> {
+  private data = [];
+  push(item: T) {
+    return this.data.push(item);
+  }
+  pop() {
+    return this.data.shift();
+  }
+}
+
+const queue = new Queue<number>();
+queue.push(1);
+// queue.push("a");  // type error
+
+const queue2 = new Queue<string>();
+queue2.push("a");
+// queue2.push(1)  // type error
+```
+
+接口使用泛型
+
+```
+interface KeyPair<T, U> {
+  key: T;
+  value: U;
+}
+
+let kp1: KeyPair<number, string> = { key: 1, value: "str" };
+let kp2: KeyPair<string, number> = { key: "test", value: 100 };
+```
+
+用泛型描述数组
+
+```
+let arrTwo: Array<number> = [1, 2, 3];
+```
+
+用泛型接口描述函数
+
+```
+interface IPlus<T> {
+  (a: T, b: T): T;
+}
+function plus(a: number, b: number): number {
+  return a + b;
+}
+const a: IPlus<number> = plus;
+
+function connect(a: string, b: string): string {
+  return a + b;
+}
+
+const b: IPlus<string> = connect;
 ```
 
 ## TS工具
