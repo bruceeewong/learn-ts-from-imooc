9271a730500d724b88783f1e63e21e72c98ee467
bruski <292803281@qq.com>
2020-10-15 17:16:38 +0800

    feat: add interface advanced part

--- interface.ts
+++ interface.ts
@@ -10,3 +10,31 @@ let boo: Person = {
 };
 
 // boo.id = 222;  // error, readonly is not allowed to change
+
+interface Radio {
+  switchRadio(trigger: boolean): void;
+}
+interface Battery {
+  checkBatteryStatus(): string;
+}
+interface RadioWithBattery extends Radio {
+  checkBatteryStatus(): string;
+}
+class Car implements Radio {
+  switchRadio(trigger: boolean): void {
+    throw new Error("Method not implemented.");
+  }
+}
+class Cellphone implements Radio, Battery {
+  checkBatteryStatus(): string {
+    throw new Error("Method not implemented.");
+  }
+  switchRadio() {}
+}
+
+class Cellphone2 implements RadioWithBattery {
+  checkBatteryStatus(): string {
+    throw new Error("Method not implemented.");
+  }
+  switchRadio() {}
+}
