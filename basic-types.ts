a374bfbcc62862ec81a9f7951ea808172ef05269
bruski <292803281@qq.com>
2020-10-15 16:26:07 +0800

    feat: section 2: learn basic types

---
+++ basic-types.ts
@@ -0,0 +1,13 @@
+// primitive types
+let isDone: boolean = false;
+
+let age: number = 20;
+let binaryNumber: number = 0b1111;
+
+let firstName: string = "boo";
+let message: string = `Hello, ${firstName}, age is ${age}`;
+
+let u: undefined = undefined;
+let n: null = null;
+
+let num: number = undefined;
